230a3113c0b9efa62a368a02cde8ac6ecc50bf29
Shane Church <shane@s-church.net>
2015-03-10 15:54:18 -0600

    Beginning to implement Angular.js front end

---
+++ Shane.Church.Web.v2015/wwwroot/app/home/home.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+
+angular.module('shane.church.home', ['ngRoute'])
+
+.config(['$routeProvider', function ($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl: '/app/home/home.html',
+		controller: 'HomeCtrl'
+	});
+}])
+
+.controller('HomeCtrl', ['$scope', function ($scope) {
+	$scope.navToggle = function () {
+		$(".nav-toggle").toggleClass("active");
+		$(".overlay-boxify").toggleClass("open");
+	};
+
+}]);
---
+++ Shane.Church.Web.v2015/wwwroot/js/app.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+
+angular.module('shane.church', [
+  'ngRoute',
+  'shane.church.home'
+]).
+config(['$routeProvider', function ($routeProvider) {
+	$routeProvider.otherwise({ redirectTo: '/' });
+}]);
